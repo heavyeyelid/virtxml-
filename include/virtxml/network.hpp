cab59be9c8a4aa923e3843828674b4286aecc7dd
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-09-15 19:19:30 +0200

    Qfix
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

---
+++ include/virtxml/network.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <type_traits>
+#include <magic_enum.hpp>
+#include "generic.hpp"
+
+namespace virtxml {
+inline namespace {
+
+enum class MacTableManager {
+    kernel,
+    libvirt,
+};
+
+template <class CRTP, template <class> class Optional = std::void_t>
+struct HasMacTableManager : public HasMore<CRTP, HasMacTableManager<CRTP, Optional>> {
+    template <std::enable_if_t<!std::is_void_v<Optional<void>>, int> = 0>[[nodiscard]] std::optional<MacTableManager> mode() const noexcept {
+        const auto attr = this->get_node()->next_attribute("macTableManager");
+        if (!attr)
+            return std::nullopt;
+        return magic_enum::enum_cast<MacTableManager>(attr->value());
+    }
+
+    template <std::enable_if_t<std::is_void_v<Optional<void>>, int> = 0>[[nodiscard]] MacTableManager mode() const noexcept {
+        return magic_enum::enum_cast<MacTableManager>(this->get_node()->next_attribute("macTableManager")->value());
+    }
+};
+
+} // namespace
+}
--- include/virtxml/storage.hpp
+++ include/virtxml/storage.hpp
@@ -27,20 +27,21 @@ enum class Format {
 };
 
 template <class CRTP, template <class> class Optional = std::void_t> struct HasFormat : public HasMore<CRTP, HasFormat<CRTP, Optional>> {
-    template <std::enable_if_t<!std::is_void_v<Optional<void>>, int> = 0>[[nodiscard]] std::optional<Format> mode() const noexcept {
-        return *magic_enum::enum_cast<Format>(this->get_node()->next_attribute("format")->value());
+    template <std::enable_if_t<!std::is_void_v<Optional<void>>, int> = 0>[[nodiscard]] std::optional<Format> format() const noexcept {
+        const auto attr = this->get_node()->next_attribute("format");
+        if (!attr)
+            return std::nullopt;
+        return magic_enum::enum_cast<Format>(attr->value());
     }
 
-    template <std::enable_if_t<std::is_void_v<Optional<void>>, int> = 0>[[nodiscard]] Format mode() const noexcept {
+    template <std::enable_if_t<std::is_void_v<Optional<void>>, int> = 0>[[nodiscard]] Format format() const noexcept {
         return magic_enum::enum_cast<Format>(this->get_node()->next_attribute("format")->value());
     }
 };
 
 struct Secret : public Node {
     [[nodiscard]] constexpr std::string_view type() const noexcept { return "passphrase"; }
-
     [[nodiscard]] Optional<Uuid> uuid() const noexcept { return Uuid{node->first_attribute("uuid")}; }
-
     [[nodiscard]] Optional<String> usage() const noexcept { return String{node->first_attribute("usage")}; }
 };
 
@@ -53,26 +54,19 @@ struct Encryption : public Node {
 
     struct Cipher : public Node {
         [[nodiscard]] String name() const noexcept { return String{node->first_attribute("name")}; }
-
         [[nodiscard]] Integral size() const noexcept { return Integral{node->first_attribute("size")}; }
-
         [[nodiscard]] Optional<String> mode() const noexcept { return String{node->first_attribute("mode")}; }
-
         [[nodiscard]] Optional<String> hash() const noexcept { return String{node->first_attribute("hash")}; }
     };
 
     struct IVGen : public Node {
         [[nodiscard]] String name() const noexcept { return String{node->first_attribute("name")}; }
-
         [[nodiscard]] Optional<String> hash() const noexcept { return String{node->first_attribute("hash")}; }
     };
 
     [[nodiscard]] Format format() const noexcept { return *magic_enum::enum_cast<Format>(node->first_attribute("format")->value()); }
-
     [[nodiscard]] NamedSpan<Secret> secrets() const noexcept { return NamedSpan<Secret>{"secret", node}; }
-
     [[nodiscard]] Optional<Cipher> cipher() const noexcept { return Cipher{node->first_node("cipher")}; }
-
     [[nodiscard]] Optional<IVGen> ivgen() const noexcept { return IVGen{node->first_node("ivgen")}; }
 };
 } // namespace virtxml::storage
