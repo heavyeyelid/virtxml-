61d3d8f9cdf6e86b0c275d34ea975ad7629caa9e
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-09-30 02:16:49 +0200

    meta: extracted virtxml into its own repository
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- include/virt_wrap.hpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by _as on 2019-02-02.
-//
-
-#pragma once
-
-#include "virt_wrap/decls.hpp"
--- include/virt_wrap/decls.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "virt_wrap/fwd.hpp"
-
-#include "virt_wrap/Connection.hpp"
-#include "virt_wrap/Domain.hpp"
-#include "virt_wrap/Network.hpp"
-#include "virt_wrap/NodeDevice.hpp"
-#include "virt_wrap/TypesParam.hpp"
